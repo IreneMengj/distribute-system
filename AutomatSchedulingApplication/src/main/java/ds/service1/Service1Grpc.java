a4e9b13e72c8e005e201f7c028219806df8079ed
irene <mengjiayu2021@gmail.com>
2023-04-01 12:20:39 +0100

    update pom.xml

--- AutomatSchedulingApplication/src/main/java/ds/service1/Service1Grpc.java
+++ AutomatSchedulingApplication/src/main/java/ds/service1/Service1Grpc.java
@@ -22,7 +22,7 @@ import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
  */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.15.0)",
-    comments = "Source: service1.proto")
+    comments = "Source: UserManagement.proto")
 public final class Service1Grpc {
 
   private Service1Grpc() {}
